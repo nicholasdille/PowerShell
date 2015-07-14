a39ce630966bcba89f0286a97395acff8b0286bc
Nicholas Dille <nicholas.dille@mailbox.org>
2015-07-14 18:45:07 +0200

    Unit test

--- Support/CSV.Tests.ps1
+++ Support/CSV.Tests.ps1
@@ -3,4 +3,28 @@ $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace('.Tests.', '.')
 . "$here\$sut"
 
 Describe 'CSV' {
+    Context 'Import-Array' {
+        It 'Imports lines into array' {
+            @(
+                'line1'
+                'line2'
+            ) | Set-Content -Path Testdrive:\input.txt
+            $data = Import-Array -File Testdrive:\input.txt
+            $data -is [array] | Should Be $true
+            $data -contains 'line1' | Should Be $true
+            $data -contains 'line2' | Should Be $true
+        }
+        It 'Chooses the correct file' {
+            @(
+                'line1'
+                'line2'
+            ) | Set-Content -Path Testdrive:\input.txt
+            @(
+                'line3'
+                'line4'
+            ) | Set-Content -Path Testdrive:\input-override.txt
+            $data = Import-Array -File Testdrive:\input-override.txt,Testdrive:\input.txt
+            $data -contains 'line3' | Should Be $true
+        }
+    }
 }
