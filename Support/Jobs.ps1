450ba0d8ccd288c18e7d999e31cf9ff0dc492207
Nicholas Dille <nicholas.dille@mailbox.org>
2015-07-15 16:26:47 +0200

    Added processing of Windows updates

--- Support/Jobs.ps1
+++ Support/Jobs.ps1
@@ -55,3 +55,111 @@
         $Jobs
     }
 }
+
+function Install-WindowsUpdate {
+    [CmdletBinding()]
+    param(
+        [Parameter()]
+        [ValidateSet('All', 'Recommended')]
+        [string]
+        $Filter = 'Recommended'
+        ,
+        [Parameter()]
+        [switch]
+        $ScanOnly
+        ,
+        [Parameter()]
+        [switch]
+        $DownloadOnly
+        ,
+        [Parameter()]
+        [ValidateSet('None', 'Shutdown', 'Reboot')]
+        [string]
+        $PowerAction = 'None'
+        ,
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
+        [int]
+        $PowerActionDelaySeconds = 5
+    )
+
+    $SearchString = @{
+        'All'         = "IsInstalled=0 and Type='Software' and AutoSelectOnWebsites=1"
+        'Recommended' = "IsInstalled=0 and Type='Software'"
+    }
+    Write-Information -Message ('[{0}] Using search string <{1}> based on filter <{2}>' -f $MyInvocation.MyCommand, $SearchString[$Filter], $Filter)
+    
+    Write-Information -Message ('[{0}] Searching for updates' -f $MyInvocation.MyCommand)
+    $UpdateSession = New-Object -Com Microsoft.Update.Session
+    $UpdateSearcher = $UpdateSession.CreateUpdateSearcher()
+    $SearchResult = $UpdateSearcher.Search($SearchString[$Filter])
+
+    if ($SearchResult.Updates.Count -eq 0) {
+        Write-Information -Message ('[{0}] No updates found' -f $MyInvocation.MyCommand)
+        return
+    }
+
+    if ($ScanOnly) {
+        Write-Information -Message ('[{0}] ScanOnly' -f $MyInvocation.MyCommand)
+        foreach ($Update in $SearchResult.Updates) {
+            Write-Host ('[{0}] Found update: {1}' -f $MyInvocation.MyCommand, $Update.Title)
+        }
+        Write-Information -Message ('[{0}] ScanOnly. Done.' -f $MyInvocation.MyCommand)
+        return
+    }
+
+    Write-Information -Message ('[{0}] Building download list' -f $MyInvocation.MyCommand)
+    $UpdatesToDownload = New-Object -Com Microsoft.Update.UpdateColl
+    foreach ($Update in $SearchResult.Updates) {
+        Write-Information -Message ('[{0}] Adding an update to the download list: {0}.' -f $MyInvocation.MyCommand, $Update.Title)
+        $UpdatesToDownload.Add($Update) | Out-Null
+    }
+    if ($UpdatesToDownload.Count -eq 0) {
+        Write-Information -Message ('[{0}] No updates found. Aborting.' -f $MyInvocation.MyCommand)
+        return
+    }
+
+    Write-Information -Message ('[{0}] Downloading updates' -f $MyInvocation.MyCommand)
+    $Downloader = $UpdateSession.CreateUpdateDownloader()
+    $Downloader.Updates = $UpdatesToDownload
+    $Downloader.Download()
+
+    if ($DownloadOnly) {
+        Write-Information -Message ('[{0}] DownloadOnly. Done.' -f $MyInvocation.MyCommand)
+        return
+    }
+
+    Write-Information -Message ('[{0}] Building installation list' -f $MyInvocation.MyCommand)
+    $UpdatesToInstall = New-Object -Com Microsoft.Update.UpdateColl
+    foreach ($Update in $SearchResult.Updates) {
+        if ($Update.IsDownloaded) {
+            Write-Information -Message ('[{0}] Adding a downloaded update to the installation list: {0}.' -f $MyInvocation.MyCommand, $Update.Title)
+            $UpdatesToInstall.Add($Update) | Out-Null
+        }
+    }
+    if ($UpdatesToInstall.Count -eq 0) {
+        Write-Information -Message ('[{0}] No updates downloaded. Aborting.' -f $MyInvocation.MyCommand)
+        return
+    }
+
+    Write-Information -Message ('[{0}] Installing updates' -f $MyInvocation.MyCommand)
+    $Installer = $UpdateSession.CreateUpdateInstaller()
+    $Installer.Updates = $UpdatesToInstall
+    $InstallationResult = $Installer.Install()
+
+    if ($InstallationResult.RebootRequired -and $PowerAction -ine 'None') {
+        Write-Information -Message ('[{0}] PowerAction' -f $MyInvocation.MyCommand)
+
+        $PowerActionParameter = ''
+        if ($PowerAction -ieq 'Shutdown') {
+            $PowerActionParameter = '/s'
+
+        } elseif ($PowerAction -ieq 'Reboot') {
+            $PowerActionParameter = '/r'
+        }
+        Write-Information -Message ('[{0}] {1} ({2}) in {3} seconds' -f $MyInvocation.MyCommand, $PowerAction, $PowerActionParameter, $PowerActionDelaySeconds)
+        shutdown.exe $PowerActionParameter /t $PowerActionDelaySeconds
+    }
+
+    Write-Information -Message ('[{0}] Done.' -f $MyInvocation.MyCommand)
+}
