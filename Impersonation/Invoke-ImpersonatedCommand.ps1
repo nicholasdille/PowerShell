c7f0c7b1d7d3f27cb5ed88b85e21887726ecfdbf
Nicholas Dille <nicholas.dille@mailbox.org>
2015-07-15 16:25:29 +0200

    Impersonation by change the current access token

---
+++ Impersonation/Invoke-ImpersonatedCommand.ps1
@@ -0,0 +1,96 @@
+﻿param( [ScriptBlock] $scriptBlock )
+<#
+    .SYNOPSIS 
+          Impersonates a user and executes a script block as that user. This is an interactive script
+        and a window will open in order to securely capture credentials.
+    .EXAMPLE
+        Use-Impersonation.ps1 {Get-ChildItem 'C:\' | Foreach { Write-Host $_.Name }}
+        This writes the contents of 'C:\' impersonating the user that is entered.
+#>
+    
+$logonUserSignature =
+@'
+[DllImport( "advapi32.dll" )]
+public static extern bool LogonUser( String lpszUserName,
+                                     String lpszDomain,
+                                     IntPtr lpszPassword,
+                                     int dwLogonType,
+                                     int dwLogonProvider,
+                                     ref IntPtr phToken );
+'@
+ 
+$AdvApi32 = Add-Type -MemberDefinition $logonUserSignature -Name 'AdvApi32' -Namespace 'PsInvoke.NativeMethods' -PassThru
+ 
+$closeHandleSignature =
+@'
+[DllImport( "kernel32.dll", CharSet = CharSet.Auto )]
+public static extern bool CloseHandle( IntPtr handle );
+'@
+ 
+$Kernel32 = Add-Type -MemberDefinition $closeHandleSignature -Name 'Kernel32' -Namespace 'PsInvoke.NativeMethods' -PassThru
+    
+$credentials = Get-Credential
+ 
+try
+{
+    $Logon32ProviderDefault = 0
+    $Logon32LogonInteractive = 2
+    $Logon32LogonNetwork = 3
+    $Logon32LogonBatch = 4
+    $Logon32LogonService = 5
+    $Logon32LogonUnlock = 7
+    $Logon32LogonNetworkCleartext = 8
+    $Logon32LogonNewCredentials = 9
+    $tokenHandle = [IntPtr]::Zero
+    $userName = Split-Path $credentials.UserName -Leaf
+    $domain = Split-Path $credentials.UserName
+    $unmanagedString = [IntPtr]::Zero;
+    $success = $false
+    
+    try
+    {
+        $unmanagedString = [System.Runtime.InteropServices.Marshal]::SecureStringToGlobalAllocUnicode($credentials.Password);
+        $success = $AdvApi32::LogonUser($userName, $domain, $unmanagedString, $Logon32LogonNewCredentials, $Logon32ProviderDefault, [Ref] $tokenHandle)
+    }
+    finally
+    {
+        [System.Runtime.InteropServices.Marshal]::ZeroFreeGlobalAllocUnicode($unmanagedString);
+    }
+    
+    if (!$success )
+    {
+        $retVal = [System.Runtime.InteropServices.Marshal]::GetLastWin32Error()
+        Write-Host "LogonUser was unsuccessful. Error code: $retVal"
+        return
+    }
+ 
+    Write-Host 'LogonUser was successful.'
+    Write-Host "Value of Windows NT token: $tokenHandle"
+ 
+    $identityName = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+    Write-Host "Current Identity: $identityName"
+ 
+    $newIdentity = New-Object System.Security.Principal.WindowsIdentity( $tokenHandle )
+    $context = $newIdentity.Impersonate()
+ 
+    $identityName = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+    Write-Host "Impersonating: $identityName"
+ 
+    Write-Host 'Executing custom script'
+    & $scriptBlock
+}
+catch [System.Exception]
+{
+    Write-Host $_.Exception.ToString()
+}
+finally
+{
+    if ( $context -ne $null )
+    {
+        $context.Undo()
+    }
+    if ( $tokenHandle -ne [System.IntPtr]::Zero )
+    {
+        $Kernel32::CloseHandle( $tokenHandle )
+    }
+}
---
+++ Impersonation/Test_ImpersonateAs.ps1
@@ -0,0 +1,65 @@
+﻿function Get-ImpersonatetLib
+{
+    if ($script:ImpersonateLib)
+    {
+        return $script:ImpersonateLib
+    }
+
+    $sig = @'
+[DllImport("advapi32.dll", SetLastError = true)]
+public static extern bool LogonUser(string lpszUsername, string lpszDomain, string lpszPassword, int dwLogonType, int dwLogonProvider, ref IntPtr phToken);
+
+[DllImport("kernel32.dll")]
+public static extern Boolean CloseHandle(IntPtr hObject);
+'@ 
+   $script:ImpersonateLib = Add-Type -PassThru -Namespace 'Lib.Impersonation' -Name ImpersonationLib -MemberDefinition $sig 
+
+   return $script:ImpersonateLib
+    
+}
+
+function ImpersonateAs([PSCredential] $cred)
+{
+    [IntPtr] $userToken = [Security.Principal.WindowsIdentity]::GetCurrent().Token
+    $userToken
+    $ImpersonateLib = Get-ImpersonatetLib
+
+    $bLogin = $ImpersonateLib::LogonUser($cred.GetNetworkCredential().UserName, $cred.GetNetworkCredential().Domain, $cred.GetNetworkCredential().Password, 
+    9, 0, [ref]$userToken)
+    
+    if ($bLogin)
+    {
+        $Identity = New-Object Security.Principal.WindowsIdentity $userToken
+        $context = $Identity.Impersonate()
+    }
+    else
+    {
+        throw "Can't Logon as User $cred.GetNetworkCredential().UserName."
+    }
+    $context, $userToken
+}
+
+function CloseUserToken([IntPtr] $token)
+{
+    $ImpersonateLib = Get-ImpersonatetLib
+
+    $bLogin = $ImpersonateLib::CloseHandle($token)
+    if (!$bLogin)
+    {
+        throw "Can't close token"
+    }
+}
+
+Write-Host 'get addomain'
+#Get-ADDomain
+Write-Host 'get cred'
+#$cred = Import-Clixml -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Cred\administrator@DEMO.clixml')
+$cred = Get-Credential
+Write-Host 'impersonate'
+($oldToken, $context, $newToken) = ImpersonateAs $cred
+Write-Host 'get addomain'
+Get-ADDomain
+Write-Host 'undo'
+$context.Undo()
+$context.Dispose()
+CloseUserToken($newToken)
