f3d9a37740af40615734af248fbe7a287508bea0
Nicholas Dille <nicholas.dille@mailbox.org>
2016-01-27 20:29:01 +0100

    Fixed to fail in CI

--- maketest.ps1
+++ maketest.ps1
@@ -10,9 +10,8 @@ $tests = Get-ChildItem -Path (Join-Path -Path $here -ChildPath 'tests/*.tests.ps
 $ConfirmPreference = 'None'
 
 foreach ($file in $tests ) {
-    $nunitxml = $file.fullname + '.nunit.result.xml'
-    $clixml = $file.fullname + '.clixml.result.xml'
-    #powershell.exe -noprofile -c "invoke-pester -path $($file.fullname) -outputFormat NUnitXml -OutputFile $nunitxml -passthru |export-clixml -path $clixml"
+    $nunitxml = $file.FullName + '.nunit.result.xml'
+    $clixml = $file.FullName + '.clixml.result.xml'
     Invoke-Pester -Path $($file.FullName) -OutputFormat NUnitXml -OutputFile $nunitxml -PassThru | Export-Clixml -Path $clixml
 }
 
@@ -28,4 +27,32 @@ If ($env:APPVEYOR_JOB_ID) {
 
 } Else {
     'Skipping Appveyor upload because this job is not running in Appveyor'
+}
+
+$Results = @( Get-ChildItem -Path (Join-Path -Path $here -ChildPath 'tests/*.clixml.result.xml') | Import-Clixml )
+
+$FailedCount = $Results |
+    Select -ExpandProperty FailedCount |
+    Measure-Object -Sum |
+    Select -ExpandProperty Sum
+
+if ($FailedCount -gt 0) {
+    $FailedItems = $Results |
+        Select -ExpandProperty TestResult |
+        Where {$_.Passed -notlike $True}
+
+    "FAILED TESTS SUMMARY:`n"
+    $FailedItems | ForEach-Object {
+        $Test = $_
+        [pscustomobject]@{
+            Describe = $Test.Describe
+            Context = $Test.Context
+            Name = "It $($Test.Name)"
+            Result = $Test.Result
+        }
+    } |
+        Sort Describe, Context, Name, Result |
+        Format-List
+
+    throw "$FailedCount tests failed."
 }
